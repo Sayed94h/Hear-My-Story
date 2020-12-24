10ff6adba44c81e48cdf2ece16b08316788c20fe
eltayeb <gaaliii@yahoo.com>
2020-12-24 01:18:02 +0100

    edit-user-model

--- api/models/User.js
+++ api/models/User.js
@@ -1,37 +1,21 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const { cryptPassword } = require('../utils/encryption');
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
+ const userSchema = new Schema({
+     name: {
+         type: String,
+         required: true
+     },
+     email: {
+         type: String,
+         required: true
+     },
+     password: {
+         type: String,
+         required: true
+     }
+ },{timestamps: true})
 
-UserSchema.pre('save', function(next) {
-  if (this.isNew || this.isModified('password')) {
-    const document = this;
-    cryptPassword(this.password).then((hash) => {
-      document.password = hash;
-      next();
-    }).catch((err) => {
-      console.log(err);
-      next();
-    });
-  } else {
-    next();
-  }
-});
+ const User = mongoose.model('User', userSchema)
 
-UserSchema.methods.isCorrectPassword = function(password, callback) {
-  bcrypt.compare(password, this.password, function(err, same) {
-    if (err) {
-      callback(err);
-    } else {
-      callback(err, same);
-    }
-  });
-}
-
-module.exports = mongoose.model('User', UserSchema); 
+ module.exports = User;
