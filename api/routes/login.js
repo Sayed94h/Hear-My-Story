6adac508d121dfc684dedea172c680e7b2f99a8f
Hazem Bittar <mr.hazem@hotmail.fr>
2020-12-31 00:55:12 +0100

    Update register - login api

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,69 +1,99 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const User = require('../models/user');
+const { auth } = require('../middleware');
 const path = require('path');
-const jwt = require('jsonwebtoken');
-const withAuth = require('../middleware');
-const User = require('../models/User');
 
-const {JWT_SECRET} = require('../config');
 
 const app = express();
+// app use
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cookieParser());
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+
+// adding new user (sign-up route)
+app.post('/register', function(req, res) {
+    // taking a user
+    const newuser = new User(req.body);
+    console.log(newuser);
+
+
+
+    User.findOne({ email: newuser.email }, function(err, user) {
+        if (user) return res.status(400).json({ auth: false, message: "email exits" });
+
+        newuser.save((err, doc) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({ success: false });
+            }
+            res.status(200).json({
+                succes: true,
+                user: doc
+            });
+        });
+    });
 });
 
-app.get('/home', function (req, res) {
-  res.send('Welcome!');
+
+// login user
+app.post('/login', function(req, res) {
+    let token = req.cookies.auth;
+    User.findByToken(token, (err, user) => {
+        if (err) return res(err);
+        if (user) return res.status(400).json({
+            error: true,
+            message: "You are already logged in"
+        });
+
+        else {
+            User.findOne({ 'email': req.body.email }, function(err, user) {
+                if (!user) return res.json({ isAuth: false, message: ' Auth failed ,email not found' });
+
+                user.comparepassword(req.body.password, (err, isMatch) => {
+                    if (!isMatch) return res.json({ isAuth: false, message: "password doesn't match" });
+
+                    user.generateToken((err, user) => {
+                        if (err) return res.status(400).send(err);
+                        res.cookie('auth', user.token).json({
+                            isAuth: true,
+                            id: user._id,
+                            email: user.email
+                        });
+                    });
+                });
+            });
+        }
+    });
 });
 
-app.get('/secret', withAuth, function (req, res) {
-  res.send('You are visiting a protected page.');
-});
-
-app.post('/register', async (req, res) => {
-  const {email, password} = req.body;
-  const user = new User({email, password});
-
-  try {
-    await user.save();
-  } catch (e) {
-    res.sendStatus(500)
-    return;
-  }
-
-  res.sendStatus(200);
-});
-
-app.post('/authenticate', async (req, res) => {
-  const {email, password} = req.body;
-
-  let user = null;
-
-  try {
-    user = await User.findOne({where: {email}});
-  } catch (e) {
-    res.sendStatus(401)
-    return;
-  }
-
-  if (!user) {
-    res.sendStatus(401)
-    return;
-  }
-
-  if (user.validPassword(password)) {
-    // Issue token
-    const payload = {email};
-    const token = jwt.sign(payload, JWT_SECRET, {
-      expiresIn: '1h'
+//logout user
+app.get('/logout', auth, function(req, res) {
+    req.user.deleteToken(req.token, (err, user) => {
+        if (err) return res.status(400).send(err);
+        res.status(200).send("See you soon!");
     });
 
-    res.cookie('token', token, {httpOnly: true}).sendStatus(200);
-  }
 });
 
-app.get('/checkToken', withAuth, function (req, res) {
-  res.sendStatus(200);
+// get logged in user
+app.get('/profile', auth, function(req, res) {
+    res.json({
+        isAuth: true,
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name
+
+    })
 });
 
-module.exports = app;
+
+app.get('/', function(req, res) {
+    res.status(200).send(`Welcome to Hear My Story App`);
+});
+
+
