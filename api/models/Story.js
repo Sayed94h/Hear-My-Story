b0fa2dc4dfa555b2ed1f44705155150ea75cc075
Abel Roland <abelroland@gmail.com>
2020-12-31 11:45:41 +0100

    Adding reaction on StorySchema

--- api/models/Story.js
+++ api/models/Story.js
@@ -4,6 +4,7 @@ const StorySchema = new mongoose.Schema({
     author : { type : Schema.Types.ObjectId, ref : 'User' },
     story: {type: String, required: true},
     created: { type: Date, default: Date.now },
+    reactions: [{ reaction: String, author: { type: Schema.Types.ObjectId, ref : 'User' }}]
   });
 
   module.exports = mongoose.model('Story', StorySchema); 
