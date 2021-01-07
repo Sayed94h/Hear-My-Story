bccd93904410ff8d6c6862284f8c3879e1155ec6
eltayeb <gaaliii@yahoo.com>
2021-01-07 19:32:21 +0100

    add-stories-controllers closes #7

---
+++ api/controllers/stories.js
@@ -0,0 +1,30 @@
+const Story = require('../models/Story');
+const User = require('../models/User')
+
+
+module.exports.createStory =  async (req, res, next) => {
+    
+    if(!req.body.story) return res.status(400).send('No text provided');
+    const story = new Story(req.body);
+    let token = req.cookies.auth;
+    
+    User.findByToken(token, (err, user) => {
+        if(err) {
+            return res(err)
+        }
+        if(user) {
+            story.author = user._id;
+            console.log(story);
+            story.save();
+
+            return res.status(200).json({
+                story  
+              });
+        }
+    })
+    
+   
+    
+}
+
+
