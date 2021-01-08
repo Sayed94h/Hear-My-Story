b9d00f4158b87dd72260dae33b670a6af5d18eb2
murielle <murvachrist@yahoo.fr>
2021-01-08 08:00:12 +0100

    remove

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,6 @@ import { Link, Route, Switch } from 'react-router-dom';
 import withAuth from './withAuth';
 import Home from './Home';
 import Secret from './Secret';
-import Registration from './registration';
-import SignIn from './UserSignIn';
-import SignUp from './UserSignUp';
 import Header from "./Header";
 
 class App extends Component {
@@ -16,10 +13,6 @@ class App extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/secret" component={withAuth(Secret)} />
-          <Route path="/registration" component={Registration} />
-          <route path="/UserSignIn" component={SignIn} />
-          <Route exact path='/signin' render={(history) => <SignIn />} />
-          <Route exact path='/signup' render={(history) => <SignUp />} />
         </Switch>
       </div>
     );
