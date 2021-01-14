ac0ef85d40b5432117a2b31ebea4fe065012a0c6
Abel Roland <abelroland@gmail.com>
2021-01-14 10:49:23 +0100

    Adding User routes

---
+++ api/routes/user.js
@@ -0,0 +1,37 @@
+const { User } = require('../models/User');
+const express = require('express');
+const router = express.Router();
+
+const updateUser = (req, res) => {
+    const { id } = req.params
+    User
+        .findOneAndUpdate(
+            { '_id': id }, 
+            { ...req.body,}, 
+            { new: true, rawResult: true },
+        )
+        .then(result => {
+            res.status(200).send(result)
+        })
+        .catch(error => res.status(400).json({ error }))
+}
+
+const deleteUser = (req, res) => {
+    const { id } = req.params;
+    User
+        .deleteOne({ _id: id })
+        .then(result => {
+            res.send(result)
+        })
+        .catch(err => res.status(400).send(err))
+    ;
+}
+
+router.put(
+    '/:id/update',
+    updateUser
+)
+
+router.delete('/:id', deleteUser);
+
+module.exports = router;
